520afbeae49dcab529a36d614c0ecd075678327d
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-08-13 12:00:57 -0700

    commit 2

--- scripts/store.js
+++ scripts/store.js
@@ -1,7 +1,13 @@
-let items = [];
-let hideCheckedItems = false;
+import item from './item.js'
+
+const items = [];
+const hideCheckedItems = false;
 
 export default {
   items,
   hideCheckedItems
-};
+}
+
+console.log(item);
+console.log(foo);
+console.log('hi');
